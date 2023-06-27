dac7e0e70b1b47a533c3fbbbdb74fbf26cc082be
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-06-26 22:23:20 -0500

    Wire Frame created for Quiz game

---
+++ script.js
@@ -0,0 +1,20 @@
+//  <!-- Blank Main Screen with welcome prompt -->
+      // -	clickable button  “ Welcome to FrontEnd-Stack Basics – Enter Game”  or 
+       // - Write a function called welcomeButton that enables player to click and Enter Game .
+    //    const welcomeButn = () => {
+    //     console.log(welcomeButn)
+    //    }
+
+
+       
+
+// -	Window-prompt for player (s) to enter name(s)
+        // - Write a function called enterName that enables the player(s) type in their name(s) .
+        let enterName = window.prompt(`Welcome to FrontEnd-Stack Basics : Enter Name to Start Game`)
+        console.log(enterName)
+// -	Write a function called nextQuestion that enables player to click and proceed to next question
+// -	Write a function called selectAnwser that allows player to click on answer of choice.
+// -	Create an array of  5 – 10 questions with multiple choice answers and  Boolean. ( Randomize the array)
+// -	Write a function called resetGame  that enables player to restart the game.
+ 
+
